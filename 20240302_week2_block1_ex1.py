348cdf8cd9292f413643b27a68ffc87883f0e0c5
EvgeniaKantor <160479668+EvgeniaKantor@users.noreply.github.com>
2024-03-02 13:37:33 +0200

    Add files via upload

---
+++ 20240302_week2_block1_ex1.py
@@ -0,0 +1,32 @@
+first = "Hello World" 
+#This is a comment
+print("I AM A COMPUTER")
+if 1<2<4:
+    print ("Math is fun.")
+result = True and False
+print(result)
+print(len("What's my length?"))
+print("i am shouting".upper())
+print(type(int('1000')))
+x = int(4)
+y = 'real'
+result = str(x)+y
+print(result)
+result1=3*'cool'
+print(result1)
+#ZeroDivisionError
+print(type([]))
+name = input("enter your name\n")
+print (name)
+_number_= float(input("Enter a number"))
+if _number_ < 0:
+    print("That number is less than 0")
+elif _number_ > 0:
+    print("That number is greater than 0")
+else:
+    print("you enetred 0")
+_word = "Apple"
+print(_word.find('l'))
+print('y' in 'xylophone')
+_word1 = "my_string"
+print(_word1.islower())
